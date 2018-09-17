7a3879b5d583197e7aa4e71dfe82f83d70608482
Alexander S <Sokolov.AAlek@gazprom-neft.ru>
2018-09-17 16:49:38 +0300

    healthcheck

--- api/src/main/java/ru/gazprom_neft/gpn_at/api/service/HealthController.java
+++ api/src/main/java/ru/gazprom_neft/gpn_at/api/service/HealthController.java
@@ -6,8 +6,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.xml.ws.Response;
-
+/**
+ * Пример контроллера жизнеспособности приложения.
+ */
 @RestController
 @RequestMapping("/monitoring")
 public class HealthController {
@@ -15,6 +16,11 @@ public class HealthController {
     @Autowired
     ApplicationState applicationState;
 
+    /**
+     * Контроль работоспособности приложения.
+     * @return - 200 Service is working properly
+     * @throws Exception 500 Service unavailable
+     */
     @RequestMapping(value = "/health", method = RequestMethod.GET)
     @ResponseBody
     public String health () throws Exception {
@@ -24,6 +30,10 @@ public class HealthController {
         return new String("Service is working properly");
     }
 
+    /**
+     * Сломать приложение, гетом, чтобы можно было дернуть из браузера
+     * @return Appliacation is broken
+     */
     @RequestMapping(value = "/brokeApp", method = RequestMethod.GET)
     @ResponseBody
     public String brokeApp (){
@@ -31,6 +41,10 @@ public class HealthController {
         return new String("Appliacation is broken");
     }
 
+    /**
+     * Восстановить приложение, гетом, чтобы прямо из браузера
+     * @return Application restored
+     */
     @RequestMapping(value = "/restoreApp", method = RequestMethod.GET)
     @ResponseBody
     public String restoreApp (){
